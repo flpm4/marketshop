052dcad56f630330e7ad005f893a7843006949f7
flpm4 <felipecabrera61@gmail.com>
2024-04-18 10:46:42 -0300

    tela de cadastro

---
+++ routes/web.php
@@ -0,0 +1,14 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/home', function () {
+    return view('welcome');
+});
+
+Route::view('/', 'home');
+
+Route::view('/cria-conta', 'cria-conta');
+
+Route::view('/testedeconteudo', 'teste');
+
