7eaa6ade0df36a2fa8806a0c80c3d8ab185c12fe
flpm4 <felipecabrera61@hotmail.com>
2024-04-25 10:50:46 -0300

    fazer botao criar conta, e festa do Samuel

--- resources/views/cria-conta.blade.php
+++ resources/views/cria-conta.blade.php
@@ -2,20 +2,22 @@
 <div class="min-h-screen flex items-center justify-center w-full dark:bg-gray-950">
 	<div class="bg-white dark:bg-gray-900 shadow-md rounded-lg px-8 py-6 max-w-md">
 		<h1 class="text-2xl font-bold text-center mb-4 dark:text-gray-200">Bem vindo!</h1>
-		<form action="#">
-			<div class="mb-4">
-				<label for="email" class="block text-sm font-medium text-gray-700 dark:text-gray-300 mb-2">Email</label>
-				<input type="email" id="email" class="shadow-sm rounded-md w-full px-3 py-2 border border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500" placeholder="seuemail@email.com" required>
-			</div>
-			<div class="mb-4">
-				<label for="email" class="block text-sm font-medium text-gray-700 dark:text-gray-300 mb-2">Nome</label>
-				<input type="emial" id="password" class="shadow-sm rounded-md w-full px-3 py-2 border border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500" placeholder="Coloque seu nome" required>
+		<form action="{{route('salva-usuario')}}" method="POST">
+		@csrf
+		<div class="mb-4">
+				<label for="text" class="block text-sm font-medium text-gray-700 dark:text-gray-300 mb-2">Nome</label>
+				<input type="text" name="Nome" id="password" class="shadow-sm rounded-md w-full px-3 py-2 border border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500" placeholder="Coloque seu nome" required>
 				<a href="#"
 					class="text-xs text-gray-600 hover:text-indigo-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"></a>
 			</div>
+			<div class="mb-4">
+				<label for="email" class="block text-sm font-medium text-gray-700 dark:text-gray-300 mb-2">Email</label>
+				<input type="email" name="Email" id="email" class="shadow-sm rounded-md w-full px-3 py-2 border border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500" placeholder="seuemail@email.com" required>
+			</div>
+			
       <div class="mb-4">
 				<label for="password" class="block text-sm font-medium text-gray-700 dark:text-gray-300 mb-2">Senha</label>
-				<input type="password" id="password" class="shadow-sm rounded-md w-full px-3 py-2 border border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500" placeholder="Coloque sua senha" required>
+				<input type="password" name="Senha" id="password" class="shadow-sm rounded-md w-full px-3 py-2 border border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500" placeholder="Coloque sua senha" required>
 				<a href="#"
 					class="text-xs text-gray-600 hover:text-indigo-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"></a>
 			</div>
@@ -25,10 +27,10 @@
 					<label for="remember" class="ml-2 block text-sm text-gray-700 dark:text-gray-300">Conectar Sempre</label>
 				</div>
 				<a href="#"
-					class="text-x text-indigo-500 hover:text-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Criar Conta
+					class="text-x text-indigo-500 hover:text-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Login
 					</a>
 			</div>
-			<button onclick="alert("hello")" type="submit" class="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Login</button>
+			<button type="submit" class="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Criar Conta</button>
 		</form>
 	</div>
 </div>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
 
 Route::get('/home', function () {
     return view('welcome');
@@ -11,4 +12,6 @@ Route::view('/', 'home');
 Route::view('/cria-conta', 'cria-conta');
 
 Route::view('/testedeconteudo', 'teste');
+
+Route::post('/salva-usuario',  function(request $request ) {dd($request);})->name('salva-usuario');
 
