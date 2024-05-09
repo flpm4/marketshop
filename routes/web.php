8543d7cd570b55339d3268de1fb639bfa7be0ae4
flpm4 <felipecabrera61@hotmail.com>
2024-05-09 10:46:27 -0300

    correção da tabela produtos, alterar for.

--- routes/web.php
+++ routes/web.php
@@ -3,12 +3,22 @@
 use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Request;
 use App\Models\User;
+use App\Models\Produto;
 
 Route::get('/home', function () {
     return view('welcome');
 });
 
-Route::view('/', 'home');
+//Route::view('/', 'home');
+
+Route::get('/', function() {
+
+    //dd(Produto::all());
+
+    $listaProdutos = Produto::all();
+
+    return view('home', compact('listaProdutos'));
+});
 
 Route::view('/cria-conta', 'cria-conta');
 
